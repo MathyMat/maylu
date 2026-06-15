687401dfc75cf19200be5acbe66bc3064919f86c
MathyMat <mathiastenemas@gmail.com>
2026-06-15 00:22:36 -0500

    fix

--- src/lib/maylu-photos.ts
+++ src/lib/maylu-photos.ts
@@ -1,7 +1,7 @@
 import puppyPet from "@/assets/maylu/maylufotos/597839975_25229931259991576_3717840230066353322_n.jpg";
 import puppyBox from "@/assets/maylu/maylufotos/598476303_25229936586657710_7890283769337584758_n.jpg";
 import portrait from "@/assets/maylu/maylufotos/maylu0.jpg";
-import polkaDot from "@/assets/maylu/maylufotos/HNI_0037.jpg";
+import polkaDot from "@/assets/maylu/maylufotos/hn.jpg";
 import puppySleep from "@/assets/maylu/maylufotos/Post de Instagram amor a los perros gracioso blanco (1).png";
 import santa from "@/assets/maylu/maylufotos/WP_20131222_15_49_43_SmartShoot.jpg";
 import adultHome from "@/assets/maylu/maylufotos/WP_20140113_002.jpg";
