6e14e3bc709d14d8be6fdae6f3ab566c2f9933b4
MathyMat <mathiastenemas@gmail.com>
2026-06-16 00:22:55 -0500

    fix

--- src/lib/maylu-config.ts
+++ src/lib/maylu-config.ts
@@ -4,7 +4,7 @@ export const CONFIG = {
   whatsappDisplay: "+51 939 266 007",
   yape: "+51 939 266 007",
   meta: 3000, // meta en soles
-  recaudado: 124, // monto recaudado actual
+  recaudado: 143, // monto recaudado actual
   dogName: "Maylu",
   dogBreed: "Viringo Peruano",
   dogAge: 13,
